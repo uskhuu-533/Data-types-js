efe958f1200426bab2c5945df24494661cb95178
24LP8255 24LP8255 <24LP8255@G301-CC0203-2.local>
2024-12-02 16:39:49 +0800

    data types

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("Hello world from js");
+
+//Data types//
+
+//1. String => Text type => "text"
+//2. Number => number type => 1, 2, ,3 ,4 ...
+//3. Boolen => true or false type => false/ true
+//4. Object => {key:value}
+//5. Array => [index, index]
+//6. Null of Undefined => no value
